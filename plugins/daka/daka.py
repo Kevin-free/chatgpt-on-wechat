e6d70886e5b23ecb9e7dd4b1f992ca9435bd19a4
kevintao1024 <kevin1258@foxmail.com>
2023-06-14 23:51:20 +0800

    update: daka

--- plugins/daka/daka.py
+++ plugins/daka/daka.py
@@ -33,8 +33,11 @@ class Daka(Plugin):
         if content.startswith("打卡"):
             e_context["context"].type = ContextType.TEXT
             msg: ChatMessage = e_context["context"]["msg"]
-            logger.debug("[Daka] content msg 打卡: %s" % msg)
-            e_context["context"].content = f'请你随机使用一种风格说一句夸奖语来鼓励用户"{msg.actual_user_nickname}"打卡健身。语言真诚风趣，字数不超过30字。你会用一种类似于人类的方式回应。你会用emoji表达情绪，如：😄😉😜。'
+            logger.debug("[Daka] content 打卡！msg: %s" % msg)
+            daka_content = msg.content.split('\n')  # 将字符串按行分割为列表
+            last_content = daka_content[-1]  # 获取最后一行
+            logger.debug("[Daka] content 打卡！last_content: %s" % last_content)
+            e_context["context"].content = f'请你随机使用一种风格说一句夸奖语来鼓励用户"{msg.actual_user_nickname}"打卡健身。并且可以根据内容{last_content}来发挥。一定要真诚！语言风趣，字数不超过30字。你会用一种类似于人类的方式回应。你会用emoji表达情绪，如：😄😉😜。'
             e_context.action = EventAction.BREAK  # 事件结束，进入默认处理逻辑
             return
 
